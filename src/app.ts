c77f599e97ce9b67f96fc75102275ae60e031866
Jake <jake.arkley@tombola.com>
2020-01-31 16:28:11 +0000

    init

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import {Application} from 'pixi.js';
+
+//TEST CODE --- TEST CODE --- TEST CODE --- TEST CODE --- TEST CODE --- TEST CODE --- 
+const app = new Application({width: 375, height: 667});
+
+document.getElementById('canvas')!.appendChild(app.view);
+
+//TEST CODE --- TEST CODE --- TEST CODE --- TEST CODE --- TEST CODE --- TEST CODE --- 
